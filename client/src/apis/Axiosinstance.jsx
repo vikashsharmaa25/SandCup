bf57040c57df94756ba777a4839f70d0a47f5fea
Vikash Sharma <Vikash9422@gmail.com>
2025-08-20 14:15:45 +0530

    add env in client side

--- client/src/apis/Axiosinstance.jsx
+++ client/src/apis/Axiosinstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
