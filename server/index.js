132eee6de62208b72c1c77d7557504f9bfd4973a
Vikash Sharma <Vikash9422@gmail.com>
2025-08-20 14:18:44 +0530

    env setup backend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
