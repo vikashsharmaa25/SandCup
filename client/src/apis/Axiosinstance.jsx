f1eebacd520ad2eed60a7947ac117c9b86161b21
Vikash Sharma <Vikash9422@gmail.com>
2025-08-20 14:49:33 +0530

    frontend url

--- client/src/apis/Axiosinstance.jsx
+++ client/src/apis/Axiosinstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://sand-cup-pw9a.vercel.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
